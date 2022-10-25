2c66755944bdd316583e74b3400fc20b135a85ab
Bruno Croci <crocidb@gmail.com>
2022-10-25 22:41:13 +0200

    program works with ><+-

--- main.rkt
+++ main.rkt
@@ -7,28 +7,30 @@
 (define (new-context x) (list (make-vector x) 0))
 
 (define (run-program code ctx) 
-    (when (non-empty-string? code) 
-        (define i (substring code 0 1))
-        (define ode (substring code 1 (string-length code)))
-        (when (valid-instructions i)
-            (run-program ode (run-instruction i ctx)))))
+    (cond 
+        [(non-empty-string? code) 
+            (define c (substring code 0 1))
+            (define ode (substring code 1 (string-length code)))
+            (cond 
+                [(valid-instructions c)
+                 (run-program ode (run-instruction c ctx))]
+                [else ctx])]
+        [else ctx]))
 
-(define (valid-instructions c) (string-contains? "<>+_[],." c))
+(define (valid-instructions c) (string-contains? "<>+-[],." c))
 
 (define (run-instruction i ctx) 
     (printf "Interpreting ~a\n" i)
     (cond
         [(string=? i ">") (cell-next ctx)]
-        [(string=? i "<") (cell-prev ctx)]))
-        ; [(= i "+") (inc-cell ctx)]
-        ; [(= i "-") (dec-cell ctx)]
-        ; [(= i ".") (print-cell ctx)]
-        ; [(= i ",") (read-cell ctx)]))
+        [(string=? i "<") (cell-prev ctx)]
+        [(string=? i "+") (cell-modify ctx (lambda (x) (+ x 1)))]
+        [(string=? i "-") (cell-modify ctx (lambda (x) (- x 1)))]))
 
 (define (cell-next ctx) (list (car ctx) (+ (car (cdr ctx)) 1)))
 (define (cell-prev ctx) (list (car ctx) (- (car (cdr ctx)) 1)))
 
-(define (modify-cell ctx p)
+(define (cell-modify ctx p)
     (define pos (car (cdr ctx)))
     (define vec (vector-copy (car ctx)))
     (define val (vector-ref vec pos))
